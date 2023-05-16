8cff789e9f7954620b458b7c793b93593bff9304
Milzam Hibatullah <hmilzam@gmail.com>
2023-05-16 13:38:50 +0700

    initial

---
+++ android/app/src/main/kotlin/com/example/flutter_gptex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_gptex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/app.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
